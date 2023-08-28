7d223065a3df94b117dd847aa02ea5f1bc5048d5
freddydordoni <freddydordoni@Mac-mini.local>
2023-08-29 08:41:58 +1000

    firebase sup

--- server/server.js
+++ server/server.js
@@ -1,2 +1,30 @@
 //! Main server file to setup and run your Express app
 //? These will define the API endpoints for user-related operations and watchlist operations, respectively. They will link routes to their respective controllers.
+
+// server/server.js
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Initialization
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample Route
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Connect to MongoDB
+const connectDB = require('./utils/db');
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,13 +3,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCjoXeOq4XztaX0IK2PlRB6UwdvFIjRWTk",
-  authDomain: "crypto-kings-23306.firebaseapp.com",
-  projectId: "crypto-kings-23306",
-  storageBucket: "crypto-kings-23306.appspot.com",
-  messagingSenderId: "54321917316",
-  appId: "1:54321917316:web:4fddebd1bba6c32068c1d1",
-  measurementId: "G-MR51CMTMMB"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID // if you have it
 };
 
   export default firebaseConfig;

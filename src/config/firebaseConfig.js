deb8f55c23890c00910c2a975ef55f7d50e5b7f4
freddydordoni <freddydordoni@Mac-mini.local>
2023-08-28 22:28:16 +1000

    setting up for transition to MERN

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,13 +3,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDRmAz7da9RhfLcfmpA5ocgKslCiIWC9Ck",
-    authDomain: "crypto-portfolio-tracker-123.firebaseapp.com",
-    projectId: "crypto-portfolio-tracker-123",
-    storageBucket: "crypto-portfolio-tracker-123.appspot.com",
-    messagingSenderId: "663114608473",
-    appId: "1:663114608473:web:5a6cae3b9e872d8c72a648",
-    measurementId: "G-D85NKF513E"
-  };
+  apiKey: "AIzaSyCjoXeOq4XztaX0IK2PlRB6UwdvFIjRWTk",
+  authDomain: "crypto-kings-23306.firebaseapp.com",
+  projectId: "crypto-kings-23306",
+  storageBucket: "crypto-kings-23306.appspot.com",
+  messagingSenderId: "54321917316",
+  appId: "1:54321917316:web:4fddebd1bba6c32068c1d1",
+  measurementId: "G-MR51CMTMMB"
+};
 
   export default firebaseConfig;
